0d3ce3b5497ff78f0d45aa2962db4814eeef9565
Matteo Ruina <matteo.ruina@gmail.com>
2021-11-29 21:56:00 +0100

    Add webhook scaffolding

---
+++ api/v1alpha1/awsauthitem_webhook.go
@@ -0,0 +1,58 @@
+/*
+Copyright 2021.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1alpha1
+
+import (
+	"k8s.io/apimachinery/pkg/runtime"
+	ctrl "sigs.k8s.io/controller-runtime"
+	logf "sigs.k8s.io/controller-runtime/pkg/log"
+	"sigs.k8s.io/controller-runtime/pkg/webhook"
+)
+
+// log is for logging in this package.
+var awsauthitemlog = logf.Log.WithName("awsauthitem-resource")
+
+func (r *AWSAuthItem) SetupWebhookWithManager(mgr ctrl.Manager) error {
+	return ctrl.NewWebhookManagedBy(mgr).
+		For(r).
+		Complete()
+}
+
+//+kubebuilder:webhook:path=/validate-aws-maruina-k8s-v1alpha1-awsauthitem,mutating=false,failurePolicy=fail,sideEffects=None,groups=aws.maruina.k8s,resources=awsauthitems,verbs=create;update,versions=v1alpha1,name=vawsauthitem.kb.io,admissionReviewVersions=v1
+
+var _ webhook.Validator = &AWSAuthItem{}
+
+// ValidateCreate implements webhook.Validator so a webhook will be registered for the type
+func (r *AWSAuthItem) ValidateCreate() error {
+	awsauthitemlog.Info("validate create", "name", r.Name)
+
+	// TODO(user): fill in your validation logic upon object creation.
+	return nil
+}
+
+// ValidateUpdate implements webhook.Validator so a webhook will be registered for the type
+func (r *AWSAuthItem) ValidateUpdate(old runtime.Object) error {
+	awsauthitemlog.Info("validate update", "name", r.Name)
+
+	// TODO(user): fill in your validation logic upon object update.
+	return nil
+}
+
+// ValidateDelete implements webhook.Validator so a webhook will be registered for the type
+func (r *AWSAuthItem) ValidateDelete() error {
+	return nil
+}
