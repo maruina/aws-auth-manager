c79c6073cbff25e9665853a41ace35703e9460aa
Matteo Ruina <matteo.ruina@gmail.com>
2023-05-29 23:16:49 +0200

    Use only goimports

--- api/v1alpha1/webhook_suite_test.go
+++ api/v1alpha1/webhook_suite_test.go
@@ -28,6 +28,7 @@ import (
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 	admissionv1beta1 "k8s.io/api/admission/v1beta1"
+
 	//+kubebuilder:scaffold:imports
 	"k8s.io/apimachinery/pkg/runtime"
 	ctrl "sigs.k8s.io/controller-runtime"
